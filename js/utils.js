4396d00dad3b619c961c7151ea0b0bda15276df1
Barath <tbarathkalyan@gmail.com>
2025-05-26 18:47:33 +0530

    ui changes

--- js/utils.js
+++ js/utils.js
@@ -24,7 +24,7 @@ const calculateProfileCompletion = (profileData) => {
 
 const updateProgressBar = (percentage) => {
   const circle = document.querySelector('.progress-ring__circle');
-  const radius = circle.r.baseVal.value;
+  const radius = circle.r.baseVal.value; // Now 44px as per the updated SVG
   const circumference = 2 * Math.PI * radius;
 
   circle.style.strokeDasharray = `${circumference} ${circumference}`;
