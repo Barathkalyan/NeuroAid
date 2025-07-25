a442d5b7b3b4fca0fb9fbb9d3edf406760c81047
kk2606 <drkrishnav06@gmail.com>
2025-07-25 23:16:06 +0530

    Vibe page UI enahnced

--- dump all files.py
+++ dump all files.py
@@ -4,7 +4,7 @@ import os
 
 # Extensions to include
 #".html", ".js", ".css", ".py", ".md"
-extensions = ["Journal.html", "journal.css"]
+extensions = ["vibe.html", "vibe.css"]
 output_file = "NEUROAID_dump.txt"
 
 with open(output_file, "w", encoding="utf-8") as out:
